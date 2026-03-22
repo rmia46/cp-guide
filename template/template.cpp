00fb1604f6029c46650068b97bb90abe009e491d
Roman Mia <mail.romanmia@gmail.com>
2026-03-22 06:10:59 +0600

    add new config files

---
+++ template/template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define FASTIO() ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define YES_NO(v) (cout << (v ? "YES\n" : "NO\n"))
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define debug(x) cerr << "\033[1;31m[DEBUG]\033[0m " << #x << " = " << (x) << " (Line: " << __LINE__ << ")" << endl;
+const int MOD = 1e9 + 7;
+
+void myrtle() {
+    
+}
+
+int main() {
+    FASTIO();
+    int t; cin >> t;
+    while(t--)
+        myrtle();
+    return 0;
+}
